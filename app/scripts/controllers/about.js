1e22ae127e9d8799c6f64893579138d808afeefb
Francisco Madrid <fjmadrid@gmail.com>
2016-05-23 17:08:00 -0400

    adding Yeoman files

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name chileAyudaMapaApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the chileAyudaMapaApp
+ */
+angular.module('chileAyudaMapaApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
